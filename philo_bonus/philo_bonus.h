dea275fca53560990d886c444c4d1c04f22b942e
Delfina Maria Luz Parada <dparada@c3r3s4.42malaga.com>
2024-07-30 16:42:49 +0200

    cosas

---
+++ philo_bonus/philo_bonus.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_nonus.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dparada <dparada@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/30 11:55:00 by dparada           #+#    #+#             */
+/*   Updated: 2024/07/30 11:55:56 by dparada          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef PHILO_BONUS_H
+# define PHILO_BONUS_H
+
+# include <stdio.h>
+# include <string.h>
+# include <unistd.h>
+# include <ctype.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <limits.h>
+# include <stdbool.h>
+# include <pthread.h>
+# include <errno.h>
+
+
+# endif
